b16d53b9f4c36418b585053f0545bac7c05c62db
Jawascript <n.jmonplaisir@gmail.com>
2023-09-04 13:23:03 -0400

    adding first Python exercise file

---
+++ Python_for_Data_Science/# Coding Exercices for week 1.py
@@ -0,0 +1,18 @@
+# Coding Exercices for week 1
+
+# String Manipulation:
+str1 = "hello"
+str2 = "world"
+
+statement = str1 + " " + str2
+print(statement)
+print(statement.upper())
+print(statement.lower())
+print(statement.title())
+
+statement2 = " how are you?"
+statement += statement2
+print(statement.split(" ")[1])
+print(statement)
+
+
