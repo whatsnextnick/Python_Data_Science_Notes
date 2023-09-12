2f0d7510a4765e44d2d975c3433de3466f03ef8b
Jawascript <n.jmonplaisir@gmail.com>
2023-09-12 13:07:25 -0400

    simplified code for 'Smash Candies' exercise

--- Python_for_Data_Science/# Coding Exercices for week 1.py
+++ Python_for_Data_Science/# Coding Exercices for week 1.py
@@ -78,16 +78,9 @@ alice_candies = 121
 bob_candies = 77
 carol_candies = 109
 
-# Your code goes here! Replace the right-hand side of this assignment with an expression
-# involving alice_candies, bob_candies, and carol_candies
-total = alice_candies + bob_candies + carol_candies
+# An expression involving alice_candies, bob_candies, and carol_candies
+(alice_candies + bob_candies + carol_candies) % 3
 
-split = total // 3
-
-to_smash = total - split * 3
-
-# Check your answer
-q4.check()
 
 
 
