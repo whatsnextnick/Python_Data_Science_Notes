4745c0a67063378fe80be2554c47e6562723993b
Jawascript <n.jmonplaisir@gmail.com>
2023-09-12 13:04:58 -0400

    adding coding exercises for numbpy

--- Python_for_Data_Science/# Coding Exercices for week 1.py
+++ Python_for_Data_Science/# Coding Exercices for week 1.py
@@ -66,7 +66,28 @@ def time_traveller():
 
 time_traveller()
 
+#Variable, and numbypy
+# Alice, Bob and Carol have agreed to pool their Halloween candy and split it evenly 
+# among themselves. For the sake of their friendship, any candies left over will be smashed. 
+# For example, if they collectively bring home 91 candies, they'll take 30 each and smash 1.
+# Write an arithmetic expression below to calculate how many candies they must smash for a given haul.
+#_______________________________________
 
+# Variables representing the number of candies collected by alice, bob, and carol
+alice_candies = 121
+bob_candies = 77
+carol_candies = 109
+
+# Your code goes here! Replace the right-hand side of this assignment with an expression
+# involving alice_candies, bob_candies, and carol_candies
+total = alice_candies + bob_candies + carol_candies
+
+split = total // 3
+
+to_smash = total - split * 3
+
+# Check your answer
+q4.check()
 
 
 
