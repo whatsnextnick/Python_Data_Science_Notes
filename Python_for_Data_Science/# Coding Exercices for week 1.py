be65c0a4e30ba61c76f9b11c81b0c31ba58c042b
Jawascript <n.jmonplaisir@gmail.com>
2023-09-14 23:38:19 -0400

    1st attempt at menu_is_boring assignment, returning [False] instead of expecting answer; False

--- Python_for_Data_Science/# Coding Exercices for week 1.py
+++ Python_for_Data_Science/# Coding Exercices for week 1.py
@@ -88,7 +88,7 @@ def menu_is_boring(meals):
     """Given a list of meals served over some period of time, return True if the
     same meal has ever been served two days in a row, and False otherwise.
     """
-
+    return [meal[meals.index(meal)] == meal[meals.index(meal)+1] for meal in meals]
  
 
 
